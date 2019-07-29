9889caa033771d0cc5b81f8429c3fc10f714d3be
Kirk Sullivan <kirk_135@hotmail.com>
2019-07-29 12:53:24 -0400

    Added Images.js for SpaceImages

---
+++ React Native Workshops/workshop-1/imageList.js
@@ -0,0 +1,32 @@
+const SpaceImages = [
+
+  {
+    url: "https://i.imgur.com/9p4VsBw.jpg",
+    caption: "When Galaxeys Collide"
+  },
+
+  {
+    url: "https://i.imgur.com/dggemYH.jpg",
+    caption: "Florida, from above."
+  },
+
+  {
+    url: "https://i.imgur.com/deI932c.jpg",
+    caption: "LEGO Spaceman"
+  },
+
+  {
+    url: "https://i.imgur.com/j69hRu8.jpg",
+    caption: "Star Wars with the crew"
+  },
+
+  {
+    url: "https://i.imgur.com/YUipl9m.jpg",
+    caption: "Japanese drone aboard the ISS"
+  },
+
+  {
+    url: "https://i.imgur.com/VcCPjaV.jpg",
+    caption: "Not the ISS ..."
+  }
+ ];
